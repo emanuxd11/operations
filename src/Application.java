cc4408083fff0f15779145eaf435dcc5f58f193b
Emanuel Maia <tavanomanu@gmail.com>
2022-09-20 14:56:37 +0100

    Is this appropriate (computer 2)

--- src/Application.java
+++ src/Application.java
@@ -4,9 +4,21 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a+b);
+        if(operation.equals("sum")) {
+            System.out.println(a + b);
+        } else if(operation.equals("subtraction")) {
+            System.out.println(a - b);
+        } else if(operation.equals("division")) {
+            System.out.println("integer division");
+            System.out.println(a / b);
+        } else if(operation.equals("multiplication")) {
+            System.out.println(a * b);
+        }
+
     }
 }
